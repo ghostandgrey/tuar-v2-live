1a0af87847102b73efdce5c7dc0098cbea017529
Peter Altosaar <peter.alrtosaar@gmail.com>
2021-02-19 11:25:24 -0500

    Change output from public IP to public DNS

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_security_group" "instance" {
   }
 }
 
-output "public_ip" {
-  value = aws_instance.example.public_ip
-  description = "The public IP address of the web server"
+output "public_dns" {
+  value = aws_instance.example.public_dns
+  description = "The public DNS address of the web server"
 }
