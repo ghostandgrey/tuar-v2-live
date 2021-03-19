69e982fab46b13092c1c02c20a2ffcbfd73cb153
Peter Altosaar <peter.alrtosaar@gmail.com>
2021-03-19 11:30:22 -0400

    Move DB state to S3 and publish address and port

--- stage/data-stores/mysql/main.tf
+++ stage/data-stores/mysql/main.tf
@@ -1,5 +1,21 @@
+terraform {
+  backend "s3" {
+    bucket = "altocorp-terraform-up-and-running-state"
+    key    = "stage/data-stores/mysql/terraform.tfstate"
+    region = "us-east-2"
+
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+
 provider "aws" {
   region = "us-east-2"
+}
+
+data "aws_secretsmanager_secret_version" "db_password" {
+  secret_id = "TUAR_DB_Password"
 }
 
 resource "aws_db_instance" "example" {
@@ -13,6 +29,12 @@ resource "aws_db_instance" "example" {
   password = jsondecode(data.aws_secretsmanager_secret_version.db_password.secret_string).dbpassword
 }
 
-data "aws_secretsmanager_secret_version" "db_password" {
-  secret_id = "TUAR_DB_Password"
+output "address" {
+  value = aws_db_instance.example.address
+  description = "Connect to the database at this endpoint"
+}
+
+output "port" {
+  value = aws_db_instance.example.port
+  description = "The port the database is listening on"
 }
