f2585ce11b6f3197a924acf6b6671497330a1b50
Peter Altosaar <peter.alrtosaar@gmail.com>
2021-03-21 16:45:53 -0400

    Specify webserver-cluster instance type and count as variables

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -3,5 +3,11 @@ provider "aws" {
 }
 
 module "webserver-cluster" {
-  source = "../../../../tuar-v2-modules/services/webserver-cluster"
+  source                 = "../../../../tuar-v2-modules/services/webserver-cluster"
+  cluster_name           = "prod"
+  db_remote_state_bucket = "altocorp-terraform-up-and-running-state"
+  db_remote_state_key    = "prod/data-stores/mysql/terraform.tfstate"
+  instance_type          = "m4.large"
+  max_size               = 2
+  min_size               = 10
 }
--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -3,5 +3,11 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-  source = "../../../../tuar-v2-modules/services/webserver-cluster"
+  source                 = "../../../../tuar-v2-modules/services/webserver-cluster"
+  cluster_name           = "stage"
+  db_remote_state_bucket = "altocorp-terraform-up-and-running-state"
+  db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
+  instance_type          = "t2.micro"
+  max_size               = 2
+  min_size               = 2
 }
